d879b4f464cdc2ca24f48004304791f7bc0e9c33
thissaram <saram1101y@gmail.com>
2025-06-12 12:26:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ def get_weather(city):
 # ----- 빵 추천 로직 -----
 def recommend_bread(weather):
     if weather == "Rain":
-        return "크루아상", "비 오는 날엔 버터 향 가득한 크루아상이 최고죠 ☔", "https://cdn.pixabay.com/photo/2020/03/20/21/04/croissant-4951889_1280.jpg"
+        return "크루아상", "비 오는 날엔 카레빵 ☔", "https://cdn.pixabay.com/photo/2020/03/20/21/04/croissant-4951889_1280.jpg"
     elif weather == "Clear":
-        return "소금빵", "맑고 청명한 날엔 담백한 소금빵! 🌞", "https://cdn.pixabay.com/photo/2022/04/06/13/57/bread-7113774_1280.jpg"
+        return "소금빵", "맑고 청명한 날엔  소금빵 🌞", "https://cdn.pixabay.com/photo/2022/04/06/13/57/bread-7113774_1280.jpg"
     elif weather == "Clouds":
-        return "앙버터", "구름 낀 날엔 달콤하고 고소한 앙버터 어때요? ☁️", "https://cdn.pixabay.com/photo/2021/05/03/16/20/bread-6225786_1280.jpg"
+        return "앙버터", "구름 낀 날엔 크림빵 ☁️", "https://cdn.pixabay.com/photo/2021/05/03/16/20/bread-6225786_1280.jpg"
     elif weather == "Snow":
         return "시나몬롤",
